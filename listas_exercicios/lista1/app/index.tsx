a469035bdfca0d704ba72850e0e06e616683e039
andremc331 <andremc331@gmail.com>
2025-08-26 19:48:50 -0300

    lista 1 has 4 exercises from lista 2

--- listas_exercicios/lista1/app/index.tsx
+++ listas_exercicios/lista1/app/index.tsx
@@ -14,78 +14,81 @@ import Nove from "./Nove";
 import Dez from "./Dez";
 import Consulta from "../screens/HistoricoScreen";
 import Historico from "../screens/ConsultaScreen";
+import { CepProvider } from "@/contexts/CepContext";
 
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <Drawer.Navigator
-      initialRouteName="Um"
-      screenOptions={({ route }) => ({
-        headerShown: true,
-        drawerActiveTintColor: "#007AFF",
-        drawerInactiveTintColor: "#555",
-        drawerLabelStyle: { fontSize: 16 },
-        drawerStyle: { backgroundColor: "#f9f9f9" },
-        drawerIcon: ({ color, size }) => {
-          let iconName: React.ComponentProps<typeof Ionicons>["name"] = "ellipse";
+    <CepProvider>
+      <Drawer.Navigator
+        initialRouteName="Um"
+        screenOptions={({ route }) => ({
+          headerShown: true,
+          drawerActiveTintColor: "#007AFF",
+          drawerInactiveTintColor: "#555",
+          drawerLabelStyle: { fontSize: 16 },
+          drawerStyle: { backgroundColor: "#f9f9f9" },
+          drawerIcon: ({ color, size }) => {
+            let iconName: React.ComponentProps<typeof Ionicons>["name"] = "ellipse";
 
-          switch (route.name) {
-            case "Um":
-              iconName = "home";
-              break;
-            case "Dois":
-              iconName = "person";
-              break;
-            case "Tres":
-              iconName = "settings";
-              break;
-            case "Quatro":
-              iconName = "images";
-              break;
-            case "Cinco":
-              iconName = "chatbox";
-              break;
-            case "Seis":
-              iconName = "document-text";
-              break;
-            case "Sete":
-              iconName = "lock-closed";
-              break;
-            case "Oito":
-              iconName = "key";
-              break;
-            case "Nove":
-              iconName = "calendar";
-              break;
-            case "Dez":
-              iconName = "checkmark-circle";
-              break;
-            case "Consulta":
-              iconName = "search";
-              break;
-            case "Historico":
-              iconName = "time";
-              break;
-          }
+            switch (route.name) {
+              case "Um":
+                iconName = "home";
+                break;
+              case "Dois":
+                iconName = "person";
+                break;
+              case "Tres":
+                iconName = "settings";
+                break;
+              case "Quatro":
+                iconName = "images";
+                break;
+              case "Cinco":
+                iconName = "chatbox";
+                break;
+              case "Seis":
+                iconName = "document-text";
+                break;
+              case "Sete":
+                iconName = "lock-closed";
+                break;
+              case "Oito":
+                iconName = "key";
+                break;
+              case "Nove":
+                iconName = "calendar";
+                break;
+              case "Dez":
+                iconName = "checkmark-circle";
+                break;
+              case "Consulta":
+                iconName = "search";
+                break;
+              case "Historico":
+                iconName = "time";
+                break;
+            }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-    >
-      <Drawer.Screen name="Um" component={Um} />
-      <Drawer.Screen name="Dois" component={Dois} />
-      <Drawer.Screen name="Tres" component={Tres} />
-      <Drawer.Screen name="Quatro" component={Quatro} />
-      <Drawer.Screen name="Cinco" component={Cinco} />
-      <Drawer.Screen name="Seis" component={Seis} />
-      <Drawer.Screen name="Sete" component={Sete} />
-      <Drawer.Screen name="Oito" component={Oito} />
-      <Drawer.Screen name="Nove" component={Nove} />
-      <Drawer.Screen name="Dez" component={Dez} />
-      <Drawer.Screen name="Consulta" component={Consulta} />
-      <Drawer.Screen name="Historico" component={Historico} />
-    </Drawer.Navigator>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Drawer.Screen name="Um" component={Um} />
+        <Drawer.Screen name="Dois" component={Dois} />
+        <Drawer.Screen name="Tres" component={Tres} />
+        <Drawer.Screen name="Quatro" component={Quatro} />
+        <Drawer.Screen name="Cinco" component={Cinco} />
+        <Drawer.Screen name="Seis" component={Seis} />
+        <Drawer.Screen name="Sete" component={Sete} />
+        <Drawer.Screen name="Oito" component={Oito} />
+        <Drawer.Screen name="Nove" component={Nove} />
+        <Drawer.Screen name="Dez" component={Dez} />
+        <Drawer.Screen name="Consulta" component={Consulta} />
+        <Drawer.Screen name="Historico" component={Historico} />
+      </Drawer.Navigator>
+    </CepProvider>
   );
 }
